fa9c60c59ce997de1f835210cf2a9c6952a14099
dani <mayor@dani.town>
2019-07-17 10:45:58 -0400

    init

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 let fetch = require('node-fetch')
+let Arweave = require('arweave/web');
 require('dotenv').config()
+
+const arweave = Arweave.init({
+    host: 'arweave.net',
+    port: 80,
+    protocol: 'https'
+});
 
 const PROPUBLICA_KEY = process.env.PROPUBLICA_KEY
 
 void async function main() {
+  let yesterdayVotes = await newBillLast24H()
+  if (yesterdayVotes) {
+    for (let i = 0; i < yesterdayVotes.length; i++) {
 
+    }
+  }
 }()
 
-async function newBill() {
+async function newBillLast24H() {
+  let res = await fetch('https://api.propublica.org/congress/v1/both/votes/recent.json', { headers: { 'X-API-Key': PROPUBLICA_KEY }})
+  let json = await res.json()
+  for (let i = 0; i < json["results"]["votes"].length; i++) {
+    let voteDate = `${json["results"]["votes"][i]["date"]} ${json["results"]["votes"][i]["time"]}`
+
+    let votesLast24H = []
+
+    // subtraction returns milliseconds and we want days so 1000 milliseconds * 60 seconds * 60 minutes * 24 hrs
+    if ((new Date() - new Date(voteDate)) / 1000 / 60 / 60 / 24) {
+      votesLast24H.push(json["results"]["votes"][i]["vote_uri"])
+    }
+    return votesLast24H
+  }
+  return false
+}
+
+
+async function getVote(voteURI) {
 
 }
