b760ab93cb28a69b3ddcb81d9bd616adb5301020
dani <mayor@dani.town>
2019-07-17 09:29:18 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+let fetch = require('node-fetch')
+require('dotenv').config()
+
+const PROPUBLICA_KEY = process.env.PROPUBLICA_KEY
+
+void async function main() {
+
+}()
+
+async function newBill() {
+
+}
